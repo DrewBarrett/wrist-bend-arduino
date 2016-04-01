69073ae96ca5a5ca8a38ee3c0370c7fe077ac8ea
Drew Barrett <dfb1998@gmail.com>
2016-04-01 10:06:46 -0400

    updated line endings for appcompat

--- bend_gryo_blue/bend_gryo_blue.ino
+++ bend_gryo_blue/bend_gryo_blue.ino
@@ -194,21 +194,21 @@ void setup() {
     // crystal solution for the UART timer.
 
     // initialize device
-    Serial.println(F("Initializing I2C devices..."));
+    Serial.println(F("Initializing I2C devices...#"));
     mpu.initialize();
 
     // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    Serial.println(F("Testing device connections...#"));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful#") : F("MPU6050 connection failed#"));
 
     // wait for ready
-    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    Serial.println(F("\nSend any character to begin DMP programming and demo: #"));
     while (Serial.available() && Serial.read()); // empty buffer
     while (!Serial.available());                 // wait for data
     while (Serial.available() && Serial.read()); // empty buffer again
 
     // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
+    Serial.println(F("Initializing DMP...#"));
     devStatus = mpu.dmpInitialize();
 
     // supply your own gyro offsets here, scaled for min sensitivity
@@ -220,16 +220,16 @@ void setup() {
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
+        Serial.println(F("Enabling DMP...#"));
         mpu.setDMPEnabled(true);
 
         // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)...#"));
         attachInterrupt(0, dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        Serial.println(F("DMP ready! Waiting for first interrupt...#"));
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
@@ -241,7 +241,7 @@ void setup() {
         // (if it's going to break, usually the code will be 1)
         Serial.print(F("DMP Initialization failed (code "));
         Serial.print(devStatus);
-        Serial.println(F(")"));
+        Serial.println(F(")#"));
     }
 
     // configure LED for output
@@ -287,7 +287,7 @@ void loop() {
     }
   }
   Serial.print("sensor: ");
-  Serial.println(flexposition);
+  Serial.println(flexposition + "#");
   //blue.print("sensor: ");
   //blue.println(flexposition);
 
@@ -316,7 +316,7 @@ void loop() {
     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
         // reset so we can continue cleanly
         mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
+        Serial.println(F("FIFO overflow!#"));
 
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (mpuIntStatus & 0x02) {
